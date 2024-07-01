5c4f02b51bafcbe90fd6510fb1128558c31969d5
Rezon669 <rezon449@gmail.com>
2024-07-01 23:22:59 +0530

    JWT-service

--- src/main/java/com/ecom/jwtservice/security/JWTUtil.java
+++ src/main/java/com/ecom/jwtservice/security/JWTUtil.java
@@ -6,11 +6,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
+
+
 
 @Component
 public class JWTUtil {
@@ -19,14 +22,15 @@ public class JWTUtil {
 	private String secret;
 
 	public String generateToken(String email, String role) throws IllegalArgumentException, JWTCreationException {
-		return JWT.create().withSubject("User Details").withIssuer("JWT-Project").withClaim("email", email)
+		return JWT.create().withSubject("User Details").withIssuer("MicroService-Project").withClaim("email", email)
 				.withClaim("role", role).withIssuedAt(new Date())
 				.withExpiresAt(new Date(System.currentTimeMillis() + 3600000)).sign(Algorithm.HMAC256(secret));
 	}
 
 	public String validateTokenAndRetrieveSubject(String token) throws JWTVerificationException {
+		System.out.println(token);
 
-		JWTVerifier verifier = JWT.require(Algorithm.HMAC256(secret)).withSubject("User Details").withIssuer("JWT-Project")
+		JWTVerifier verifier = JWT.require(Algorithm.HMAC256(secret)).withSubject("User Details").withIssuer("MicroService-Project")
 				.acceptExpiresAt(System.currentTimeMillis()).build();
 
 		DecodedJWT jwt = verifier.verify(token);
@@ -42,3 +46,4 @@ public class JWTUtil {
 		return jwt.getClaim("email").asString();
 	}
 }
+
